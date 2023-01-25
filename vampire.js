1baddb70f1c34e9ceb6ce19e82b166d3a65205ef
zachmanac <zachstephenson94@gmail.com>
2023-01-25 13:42:24 -0800

    add solutions to functions

--- vampire.js
+++ vampire.js
@@ -10,23 +10,50 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let numberofVampires = 0;
+    let currentVampire = this;
 
+    while (currentVampire.creator) {
+      currentVampire = currentVampire.creator;
+      numberofVampires++;
+    }
+    return numberofVampires;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    if (this.offspring.length > vampire.offspring.length) {
+      return true;
+    }
+    return false;
   }
+
+  // Returns the vampire object with that name, or null if no vampire exists with that name
+  vampireWithName(name) {
+    
+  }
+  
+  // Returns the total number of vampires that exist
+  get totalDescendents() {
+  
+  }
+    
+  // Returns an array of all the vampires that were converted after 1980
+  get allMillennialVampires() {
+  
+  }
+  
 
   /** Stretch **/
 
@@ -36,7 +63,14 @@ class Vampire {
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
-
+    console.log(this.name, this.numberOfVampiresFromOriginal, "this", vampire.name, vampire.numberOfVampiresFromOriginal, "vampire")
+    if (this.numberOfVampiresFromOriginal === 1) {
+      return vampire.creator;
+    }
+    if (this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal) {
+      return this;
+    }
+    return vampire;
   }
 }
 
