2f19c3d9f873976d54d49d941aefc289331b7efe
zachmanac <zachstephenson94@gmail.com>
2023-01-25 16:12:08 -0800

    fix duplicate lines issue

--- vampire.js
+++ vampire.js
@@ -43,38 +43,6 @@ class Vampire {
 
   // Returns the vampire object with that name, or null if no vampire exists with that name
   vampireWithName(name) {
-    if (this.name == name) {
-      return this;
-    }
-  }
-  
-  // Returns the total number of vampires created by that vampire
-  get numberOfOffspring() {
-    return this.offspring.length;
-  }
-
-  // Returns the number of vampires away from the original vampire this vampire is
-  get numberOfVampiresFromOriginal() {
-    let numberofVampires = 0;
-    let currentVampire = this;
-
-    while (currentVampire.creator) {
-      currentVampire = currentVampire.creator;
-      numberofVampires++;
-    }
-    return numberofVampires;
-  }
-
-  // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
-  isMoreSeniorThan(vampire) {
-    if (this.offspring.length > vampire.offspring.length) {
-      return true;
-    }
-    return false;
-  }
-
-  // Returns the vampire object with that name, or null if no vampire exists with that name
-  vampireWithName(name) {
     
     let found = null;
 
@@ -109,17 +77,6 @@ class Vampire {
     }
     return arr;
   }
-  
-  // Returns the total number of vampires that exist
-  get totalDescendents() {
-  
-  }
-    
-  // Returns an array of all the vampires that were converted after 1980
-  get allMillennialVampires() {
-  
-  }
-  
 
   /** Stretch **/
 
