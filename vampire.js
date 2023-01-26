2012711e9572d02974d022a89f3848518462f204
zachmanac <zachstephenson94@gmail.com>
2023-01-25 16:07:59 -0800

    fix branch merge issues

--- vampire.js
+++ vampire.js
@@ -8,43 +8,36 @@ class Vampire {
 
   /** Simple tree methods **/
 
-<<<<<<< HEAD
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
     this.offspring.push(vampire);
     vampire.creator = this;
-=======
-// Adds the vampire as an offspring of this vampire
-addOffspring(vampire) {
-  this.offspring.push(vampire);
-  vampire.creator = this;
-}
-
-// Returns the total number of vampires created by that vampire
-get numberOfOffspring() {
-  return this.offspring.length;
-}
-
-// Returns the number of vampires away from the original vampire this vampire is
-get numberOfVampiresFromOriginal() {
-  let numberofVampires = 0;
-  let currentVampire = this;
-
-  while (currentVampire.creator) {
-    currentVampire = currentVampire.creator;
-    numberofVampires++;
   }
-  return numberofVampires;
-}
 
-// Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
-isMoreSeniorThan(vampire) {
-  if (this.offspring.length > vampire.offspring.length) {
-    return true;
->>>>>>> traversal
+  // Returns the total number of vampires created by that vampire
+  get numberOfOffspring() {
+    return this.offspring.length;
   }
-  return false;
-}
+
+  // Returns the number of vampires away from the original vampire this vampire is
+  get numberOfVampiresFromOriginal() {
+    let numberofVampires = 0;
+    let currentVampire = this;
+
+    while (currentVampire.creator) {
+      currentVampire = currentVampire.creator;
+      numberofVampires++;
+    }
+    return numberofVampires;
+  }
+
+  // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
+  isMoreSeniorThan(vampire) {
+    if (this.offspring.length > vampire.offspring.length) {
+      return true;
+    }
+    return false;
+  }
 
   /** Tree traversal methods **/
 
@@ -53,8 +46,8 @@ isMoreSeniorThan(vampire) {
     if (this.name == name) {
       return this;
     }
-
-<<<<<<< HEAD
+  }
+  
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
     return this.offspring.length;
@@ -83,7 +76,6 @@ isMoreSeniorThan(vampire) {
   // Returns the vampire object with that name, or null if no vampire exists with that name
   vampireWithName(name) {
     
-=======
     let found = null;
 
     for (const child of this.offspring) {
@@ -116,7 +108,6 @@ isMoreSeniorThan(vampire) {
       arr = arr.concat(children);
     }
     return arr;
->>>>>>> traversal
   }
   
   // Returns the total number of vampires that exist
