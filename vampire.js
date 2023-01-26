6950fcf16048a13ff774b91e76e824fb21f475b5
zachmanac <zachstephenson94@gmail.com>
2023-01-25 16:00:01 -0800

    add traverse recursion func solutions

--- vampire.js
+++ vampire.js
@@ -8,41 +8,77 @@ class Vampire {
 
   /** Simple tree methods **/
 
-  // Adds the vampire as an offspring of this vampire
-  addOffspring(vampire) {
+// Adds the vampire as an offspring of this vampire
+addOffspring(vampire) {
+  this.offspring.push(vampire);
+  vampire.creator = this;
+}
 
+// Returns the total number of vampires created by that vampire
+get numberOfOffspring() {
+  return this.offspring.length;
+}
+
+// Returns the number of vampires away from the original vampire this vampire is
+get numberOfVampiresFromOriginal() {
+  let numberofVampires = 0;
+  let currentVampire = this;
+
+  while (currentVampire.creator) {
+    currentVampire = currentVampire.creator;
+    numberofVampires++;
   }
+  return numberofVampires;
+}
 
-  // Returns the total number of vampires created by that vampire
-  get numberOfOffspring() {
-
+// Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
+isMoreSeniorThan(vampire) {
+  if (this.offspring.length > vampire.offspring.length) {
+    return true;
   }
-
-  // Returns the number of vampires away from the original vampire this vampire is
-  get numberOfVampiresFromOriginal() {
-
-  }
-
-  // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
-  isMoreSeniorThan(vampire) {
-
-  }
+  return false;
+}
 
   /** Tree traversal methods **/
 
   // Returns the vampire object with that name, or null if no vampire exists with that name
   vampireWithName(name) {
-    
+    if (this.name == name) {
+      return this;
+    }
+
+    let found = null;
+
+    for (const child of this.offspring) {
+      found = found || child.vampireWithName(name);
+    }
+    return found;
   }
 
   // Returns the total number of vampires that exist
   get totalDescendents() {
-    
+    let count = 0;
+
+    for (const child of this.offspring) {
+      //initial 1 count for the child itself
+      count += 1;
+      count += child.totalDescendents;
+    }
+    return count;
   }
 
   // Returns an array of all the vampires that were converted after 1980
   get allMillennialVampires() {
-    
+    let arr = [];
+    if (this.yearConverted > 1980) {
+      arr.push(this);
+    }
+
+    for (const child of this.offspring) {
+      const children = child.allMillennialVampires;
+      arr = arr.concat(children);
+    }
+    return arr;
   }
 
   /** Stretch **/
